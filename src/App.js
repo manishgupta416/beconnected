83816074dc52bcaa1b5b5c3472a1e2818169aaa3
Manish <manish.info2020@gmail.com>
2023-06-23 19:53:54 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
 import Home from "./pages/Home/Home";
+import Sign from "./pages/Sign/Sign";
+import SignUp from "./pages/SignUp/SignUp";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/api-test" element={<Mockman />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Sign />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </>
   );
