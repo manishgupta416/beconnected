fc732a3ecf15f6721100c117fdd02dc4d2749af2
Manish <manish.info2020@gmail.com>
2023-07-28 12:07:26 +0530

    added explore page and route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp/SignUp";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageNotFound from "./pages/404/404";
+import Explore from "./pages/Explore/Explore";
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
     </>
   );
---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { DataContext } from "../../context/DataContext";
+import SinglePost from "../../components/SinglePost/SinglePost";
+import Navbar from "../../components/Navbar/Navbar";
+import LeftPanel from "../../components/LeftPanel/LeftPanel";
+import RightPanel from "../../components/RightPanel/RightPanel";
+import "./Explore.css";
+
+const Explore = () => {
+  const { dataState } = useContext(DataContext);
+  return (
+    <div>
+      <div className="main-container">
+        <div className="nav">
+          <Navbar />
+        </div>
+        <div className="left-side">
+          <LeftPanel />
+        </div>
+        <div className="content">
+          {dataState.posts.map((post) => (
+            <SinglePost post={post} key={post._id} />
+          ))}
+        </div>
+        <div className="right-side">
+          <RightPanel />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
