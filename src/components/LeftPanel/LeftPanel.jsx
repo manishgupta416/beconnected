42bd832404bbdcaac298b8c9ca55bf0e6410993e
Manish <manish.info2020@gmail.com>
2023-07-28 11:18:37 +0530

    fix user details

--- src/components/LeftPanel/LeftPanel.jsx
+++ src/components/LeftPanel/LeftPanel.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./LeftPanel.css";
+import { AuthContext } from "../../context/AuthContext";
+import AddPost from "../AddPost/AddPost";
 
 const LeftPanel = () => {
+  const { currentUser, loginToken } = useContext(AuthContext);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  const handleDialogOutsideClick = () => {
+    setDialogOpen(false);
+  };
+  const handleAddPost = () => {
+    setDialogOpen(true);
+  };
   return (
     <div>
       <div className="sidebar1">
@@ -27,15 +38,57 @@ const LeftPanel = () => {
             <div className="profile">Profile</div>
           </NavLink>
         </div>
-        <button className="btn-create-post">Create New Post</button>
+        <button className="btn-create-post" onClick={handleAddPost}>
+          Create New Post
+        </button>
         <div className="user-profile">
-          <div className="user-img"></div>
+          <div className="user-img">
+            <img className="user-img" src={currentUser.avatarUrl} alt="" />
+          </div>
           <div className="user-details">
-            <div className="user-name">Manish</div>
-            <div className="user-email">manish.info2020@gmail.com</div>
+            <div className="user-name">
+              {currentUser.firstName} {currentUser.lastName}
+            </div>
+            <div className="user-email">@{currentUser.username}</div>
           </div>
         </div>
       </div>
+      {isDialogOpen && (
+        <div
+          className="addNewPostDiv"
+          style={{
+            position: "sticky",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            // background: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={handleDialogOutsideClick}
+        >
+          <div
+            style={{
+              background: "#fff",
+              padding: "16px",
+              borderRadius: "8px",
+              boxShadow: "0 0 16px rgba(0, 0, 0, 0.3)",
+              maxWidth: "240px",
+              position: "absolute",
+              right: "25px",
+              top: "6px",
+            }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="addNewPostDiv">
+              {" "}
+              <AddPost />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
